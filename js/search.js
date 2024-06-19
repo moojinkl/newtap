0e3c16255710520301ff5d048a73c95e3ce0abbb
yeji.choi <moojinkl@gmail.com>
2024-06-19 17:06:16 +0900

    검색바 : 검색 기능 및 스타일 적용

---
+++ js/search.js
@@ -0,0 +1,17 @@
+const searchInput = document.getElementById("search-input");
+
+const showSearchResult = () => {
+  let searchWord = searchInput.value;
+  //   사파리 검색 링크
+  window.location.href = `https://www.google.com/search?&q=${searchWord}`;
+  searchWord = "";
+};
+
+const enterKey = (event) => {
+  if (event.code === "Enter") {
+    showSearchResult();
+  }
+};
+searchInput.addEventListener("keypress", (event) => {
+  enterKey(event);
+});
