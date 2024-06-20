63358495933ddd3a31632848104a4e752805c4e6
yeji.choi <moojinkl@gmail.com>
2024-06-20 19:44:17 +0900

    js : 즉시 실행 함수 추가

--- js/clock.js
+++ js/clock.js
@@ -1,42 +1,45 @@
-const dateElement = document.getElementById("date");
-const timeElement = document.getElementById("time");
+// 즉시 실행 함수를 사용하여 함수스코프를 지정해주어 다른 자바스크립트 파일에서는 접근할 수 없게 된다.
+(function () {
+  const dateElement = document.getElementById("date");
+  const timeElement = document.getElementById("time");
 
-const modifyNumber = (number) => {
-  return parseInt(number) < 10 ? "0" + number : number;
-};
-const getNowDate = () => {
-  const nowDate = new Date();
-  const week = [
-    "일요일",
-    "월요일",
-    "화요일",
-    "수요일",
-    "목요일",
-    "금요일",
-    "토요일",
-  ];
+  const modifyNumber = (number) => {
+    return parseInt(number) < 10 ? "0" + number : number;
+  };
+  const getNowDate = () => {
+    const nowDate = new Date();
+    const week = [
+      "일요일",
+      "월요일",
+      "화요일",
+      "수요일",
+      "목요일",
+      "금요일",
+      "토요일",
+    ];
 
-  let month = modifyNumber(nowDate.getMonth() + 1);
-  let date = modifyNumber(nowDate.getDate());
-  let day = nowDate.getDay();
-  setNowDate(month, date, week[day]);
-};
+    let month = modifyNumber(nowDate.getMonth() + 1);
+    let date = modifyNumber(nowDate.getDate());
+    let day = nowDate.getDay();
+    setNowDate(month, date, week[day]);
+  };
 
-const setNowDate = (month, date, day) => {
-  dateElement.textContent = `${month}월 ${date}일 ${day}`;
-};
+  const setNowDate = (month, date, day) => {
+    dateElement.textContent = `${month}월 ${date}일 ${day}`;
+  };
 
-const getNowTime = () => {
-  const nowDate = new Date();
-  let hour = modifyNumber(nowDate.getHours());
-  let minute = modifyNumber(nowDate.getMinutes());
-  setNowTime(hour, minute);
-};
+  const getNowTime = () => {
+    const nowDate = new Date();
+    let hour = modifyNumber(nowDate.getHours());
+    let minute = modifyNumber(nowDate.getMinutes());
+    setNowTime(hour, minute);
+  };
 
-const setNowTime = (hour, minute) => {
-  timeElement.textContent = `${hour} : ${minute}`;
-};
-getNowDate();
-getNowTime();
+  const setNowTime = (hour, minute) => {
+    timeElement.textContent = `${hour} : ${minute}`;
+  };
+  getNowDate();
+  getNowTime();
 
-setInterval(getNowTime, 1000);
+  setInterval(getNowTime, 1000);
+})();
--- js/search.js
+++ js/search.js
@@ -1,17 +1,19 @@
-const searchInput = document.getElementById("search-input");
+(function () {
+  const searchInput = document.getElementById("search-input");
 
-const showSearchResult = () => {
-  let searchWord = searchInput.value;
-  //   사파리 검색 링크
-  window.location.href = `https://www.google.com/search?&q=${searchWord}`;
-  searchWord = "";
-};
+  const showSearchResult = () => {
+    let searchWord = searchInput.value;
+    //   사파리 검색 링크
+    window.location.href = `https://www.google.com/search?&q=${searchWord}`;
+    searchWord = "";
+  };
 
-const enterKey = (event) => {
-  if (event.code === "Enter") {
-    showSearchResult();
-  }
-};
-searchInput.addEventListener("keypress", (event) => {
-  enterKey(event);
-});
+  const enterKey = (event) => {
+    if (event.code === "Enter") {
+      showSearchResult();
+    }
+  };
+  searchInput.addEventListener("keypress", (event) => {
+    enterKey(event);
+  });
+})();
